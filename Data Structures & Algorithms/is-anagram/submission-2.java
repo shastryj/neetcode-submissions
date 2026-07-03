f328655ea92638d06e52a2a73ef41377ca09b308
AchyutaShastry <71545554+shastryj@users.noreply.github.com>
2026-07-03 17:17:44 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] map = new int[26];
+        if(s.length() != t.length()){
+            return false;
+        }
+        for(int i=0; i<s.length(); i++){
+            map[s.charAt(i) - 'a']++;
+            map[t.charAt(i) - 'a']--;
+        }
+
+        for(int val: map){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
