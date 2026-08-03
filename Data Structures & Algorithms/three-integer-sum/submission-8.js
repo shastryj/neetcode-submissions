d9df3ce540fe4d849037f5665dc55ca9171af893
AchyutaShastry <71545554+shastryj@users.noreply.github.com>
2026-08-03 14:27:24 +0530

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a,b)=>a-b)
+        let res = []
+        for (let i=0; i<nums.length; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue
+            let l = i+1
+            let r = nums.length -1
+            while(l<r){
+                let sum = nums[i] + nums[l] + nums[r]
+                if(sum === 0){
+                    res.push([nums[i], nums[l], nums[r]])
+                    l++
+                    r--
+                    while(l<r && nums[l] === nums[l-1]) l++
+                }
+                else if (sum > 0){
+                    r--
+                }
+                else {
+                    l++
+                }
+            }
+        }
+        return res
+
+    }
+}
